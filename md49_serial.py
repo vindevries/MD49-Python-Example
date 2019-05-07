a257f43d5ea4b94dca453f999ba9e4837607afa9
vindevries <vincent@ivworks.com>
2019-05-07 11:30:55 +0200

    Update md49_serial.py

--- md49_serial.py
+++ md49_serial.py
@@ -6,7 +6,6 @@ port  = serial.Serial("/dev/ttyS0", baudrate=9600, parity=serial.PARITY_NONE, st
 def writeBytes(bytes):
         port.write(bytes)
 
-
 def RESET_ENCODERS():
         writeBytes("\x00\x35")
 
@@ -14,23 +13,34 @@ def SET_MODE(mode):
         strMode = struct.pack("B",mode)
         writeBytes("\x00\x34" + strMode)
 
-def GET_ENCODER_1():
-        writeBytes("\x00\x23")
-        bytes = port.read(4)
-        encoderValue = struct.unpack('L',bytes)
-        print(str(encoderValue))
-
 def SET_SPEED_1(speed):
         strSpeed = struct.pack("B",speed)
         writeBytes("\x00\x31" + strSpeed)
-
 
 def SET_SPEED_2(speed):
         strSpeed = struct.pack("B",speed)
         writeBytes("\x00\x32" + strSpeed)
 
+def GET_ENCODER_1():
+        writeBytes("\x00\x23")
+        encByte = struct.unpack("B",port.read())
+        encByte += struct.unpack("B",port.read())
+        encByte += struct.unpack("B",port.read())
+        encByte += struct.unpack("B",port.read())
+        encoder = (encByte[0] << 24) + (encByte[1] << 16) + (encByte[2] << 8) + encByte[3]
+        return encoder
+
+def GET_ENCODER_2():
+        writeBytes("\x00\x24")
+        encByte = struct.unpack("B",port.read())
+        encByte += struct.unpack("B",port.read())
+        encByte += struct.unpack("B",port.read())
+        encByte += struct.unpack("B",port.read())
+        encoder = (encByte[0] << 24) + (encByte[1] << 16) + (encByte[2] << 8) + encByte[3]
+        return encoder
 
 SET_MODE(0)
-RESET_ENCODERS()
 SET_SPEED_1(190)
-GET_ENCODER_1()
+SET_SPEED_2(190)
+print GET_ENCODER_1()
+
